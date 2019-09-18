02088857f20df5f2896bfb6d6856e3cec39cf61c
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-09-18 17:31:18 +0200

    aggiunto template

--- index.php
+++ index.php
@@ -12,6 +12,10 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.0/handlebars.min.js" charset="utf-8"></script>
     <!-- TEMPLATE-->
     <script id="pagamento-row" type="text/x-handlebars-template">
+    <li class='pagamento-item' data-id={{id}}>
+    id prestazione: {{prenotazione_id}} 
+    id pagante: {{pagante_id}} 
+    somma: {{price}}</li>
     </script>
 
     <!-- CSS: MY STYLE -->
@@ -21,18 +25,29 @@
     <title>Pagamenti</title>
   </head>
   <body> 
+
+    
     <h1>PAGAMENTI HOTEL</h1>
 
     <div class="container">
       <h2>REJECTED</h2>
+      <div class="pagamenti-container" id='rejected'>
+        <ul></ul>
+      </div>
     </div>
 
   <div class="container">
     <h2>PENDING</h2>
+    <div class="pagamenti-container" id='pending'>
+        <ul></ul>
+    </div>
   </div>
 
   <div class="container">
     <h2>ACCEPTED</h2>
+    <div class="pagamenti-container" id='accepted'>
+        <ul></ul>
+    </div>
   </div>
   </body>
 </html>
