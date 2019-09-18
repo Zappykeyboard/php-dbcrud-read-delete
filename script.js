8a217f58a0773ed60bd893a038885ae90df42aab
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-09-18 17:22:21 +0200

    aggiunto ajax

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 function init() {
-  console.log("Hello World");
+  //console.log("Hello World");
+  getPagamenti();
 }
 
 $(document).ready(init);
+
+//ottengo i pagamenti
+function getPagamenti(){
+
+  $.ajax({
+    url: 'pagamenti.php',
+    method: 'GET',
+    success: function(data){
+      console.log(data);
+    },
+    error: function(err){
+      console.log(err);
+    }
+  })
+}
+
+//inserisco i pagamenti
+function printPagamenti(data){
+
+}
+
+//rimuovo i pagamenti
+function deletePagamento(){
+
+}
